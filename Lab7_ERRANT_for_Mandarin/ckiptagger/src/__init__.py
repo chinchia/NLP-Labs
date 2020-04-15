fa4d4d5cfa109ef7821346798ee4228a943520a6
GingerTsai <g102207429@gmail.com>
2020-04-15 16:19:55 +0800

    Add files via upload

---
+++ Lab7_ERRANT_for_Mandarin/ckiptagger/src/__init__.py
@@ -0,0 +1,5 @@
+from ckiptagger.src.api import construct_dictionary
+from ckiptagger.src.api import WS
+from ckiptagger.src.api import POS
+from ckiptagger.src.api import NER
+from ckiptagger import data_utils
